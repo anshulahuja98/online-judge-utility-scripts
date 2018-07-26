1c27c79cf56fd9e64e6f0aa6b9823cf81767f168
Anshul Ahuja <anshul.ahu@gmail.com>
2018-07-27 02:12:39 +0530

    initial commit

---
+++ automatic-testing.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import os
+import html2text
+import re
+from time import sleep
+quote_page = "http://codeforces.com/contest/1011/problem/A"
+page = urlopen(quote_page)
+soup = BeautifulSoup(page,'html.parser')
+q_block = soup.find("div",class_='sample-test')
+input_blocks = soup.find_all("div",class_='input')
+output_blocks = soup.find_all("div",class_='output')
+
+os.system("g++ A.cpp -o dummy_prog.exe")
+
+for i,j in zip(input_blocks,output_blocks):
+	inputs = str(i.find("pre"))
+	inputs = inputs.replace('<br/>','\n').replace('<pre>','\n').replace('</pre>','\n')
+	# print(inputs)
+	f = open('inputs.txt','w+')
+	f.write(inputs)
+	f.close()
+	
+	outputs = str(j.find("pre"))
+	outputs = outputs.replace('<br/>','\n').replace('<pre>','\n').replace('</pre>','\n')
+	# print(outputs)
+	outputs = outputs.replace('\n','')
+
+	os.system("./dummy_prog.exe < inputs.txt >outputs.txt")
+
+	with open('outputs.txt','r') as ff:
+		data = ff.read()
+		if(data.replace('\n','')==outputs.replace('\n','')):
+			print("Correct")
+		else:
+			print("Incorrect")
+			print(data)
+			print(outputs)
+
+
+
