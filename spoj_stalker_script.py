48d0f01912de4a9510c2530608288ad4a3df2abc
Anshul Ahuja <36476228+anshulahuja98@users.noreply.github.com>
2018-08-21 15:08:50 +0530

    sanitized data to fix sort error

--- spoj_stalker_script.py
+++ spoj_stalker_script.py
@@ -26,13 +26,14 @@ for problem in q_list:
 	problem_base_url = "https://www.spoj.com/problems/"
 	problem_url = problem_base_url+problem+'/'
 	print(problem,date,problem_url)
-	q_date_list.append([problem,date,problem_url])
+	if '<' not in [problem,date,problem_url]:
+		q_date_list.append([problem,date,problem_url])
 	
 
 q_date_list = sorted(q_date_list, key=lambda y: (int(y[1][0:4]),int(y[1][5:7]),int(y[1][8:10])))
 
 f = open('datewise_problems_%s.txt'%user,'w+')
-for problem in q_date_list:
+for problem in q_date_list: 
 	f.write(problem[2]+ " " + problem[1]+'\n')
 f.close()
 
